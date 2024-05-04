cba7d5b9da16aff98b6a3b7f4d4b4ddc1ef1a051
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-05 00:53:05 +0530

    update

--- src/main/java/com/example/uttarakhand/authentication/EmailValidator.java
+++ src/main/java/com/example/uttarakhand/authentication/EmailValidator.java
@@ -1,17 +1,13 @@
 package com.example.uttarakhand.authentication;
 
 import org.springframework.stereotype.Service;
-
 import java.util.function.Predicate;
 
 @Service
 public class EmailValidator implements Predicate<String> {
 
-
     @Override
     public boolean test(String email) {
-        // TODO: REGEX to validate email
-
         boolean checkForValidEmail = email.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
         return checkForValidEmail;
     }
