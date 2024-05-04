13495cb04640ae87f44dbb06bf520cdd062fc0ef
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-05 00:58:28 +0530

    Update

--- src/main/java/com/example/uttarakhand/authentication/service/LoginService.java
+++ src/main/java/com/example/uttarakhand/authentication/service/LoginService.java
@@ -1,18 +1,13 @@
 package com.example.uttarakhand.authentication.service;
 
-import com.example.uttarakhand.authentication.LoginRequest;
-import com.example.uttarakhand.user.User;
-import com.example.uttarakhand.user.UserRepository;
 import org.springframework.stereotype.Service;
-
-
-
+import com.example.uttarakhand.user.UserRepository;
+import com.example.uttarakhand.authentication.LoginRequest;
 
 @Service
 public class LoginService {
 
     private final UserRepository userRepository;
-
     public LoginService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -21,10 +16,7 @@ public class LoginService {
         boolean isFound = userRepository.findByEmail(request.getUsername()).isPresent();
 //        boolean isMatch = userRepository.getPasswordWhereEmail(request.getUsername()).equals(request.getPassword());
 
-        if (!(isFound)) {
-            return "User not found";
-        }
-
+        if (!(isFound)) return "User not found";
 
         return "Login successfully";
     }
