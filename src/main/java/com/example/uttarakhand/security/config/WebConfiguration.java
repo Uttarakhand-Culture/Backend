f48aa7abb1dd0626508966ebc364560b339f1fdf
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-06 23:10:48 +0530

    Update

--- src/main/java/com/example/uttarakhand/security/config/WebConfiguration.java
+++ src/main/java/com/example/uttarakhand/security/config/WebConfiguration.java
@@ -1,45 +1,28 @@
 package com.example.uttarakhand.security.config;
 
-import com.example.uttarakhand.user.UserService;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.uttarakhand.user.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 
 @Configuration
 @EnableWebSecurity(debug = true) // Do not use (debug=true) in a production system! as this contain sensitive information.
 public class WebConfiguration {
 
     private final UserService userService;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     public WebConfiguration(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//    @Bean
-//    AuthenticationProvider authenticationProvider (){
-//
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//
-//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-//        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-//        return daoAuthenticationProvider;
-//    }
 
 
     @Bean
@@ -48,9 +31,8 @@ public class WebConfiguration {
                 authorizeHttpRequests((authorize) -> authorize
                         // make sure it is in order to access the proper Url
                           .requestMatchers("/api/v*/registration/**").permitAll()
-                                .requestMatchers("/login").permitAll()
-                        .requestMatchers("/home").permitAll()
-
+                          .requestMatchers("/login").permitAll()
+                          .requestMatchers("/home").permitAll()
 
 //                        .requestMatchers(HttpMethod.POST,"/register").permitAll()
 //                        .requestMatchers("/home").permitAll()
