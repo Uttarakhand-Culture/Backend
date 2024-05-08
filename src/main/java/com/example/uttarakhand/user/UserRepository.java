9243a9a2fc8181d0f395cc5ac890d60f15982c4c
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-08 16:58:45 +0530

    Update

--- src/main/java/com/example/uttarakhand/user/UserRepository.java
+++ src/main/java/com/example/uttarakhand/user/UserRepository.java
@@ -1,20 +1,17 @@
 package com.example.uttarakhand.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-//    String getPasswordWhereEmail(String email);
 
     @Transactional
     @Modifying
