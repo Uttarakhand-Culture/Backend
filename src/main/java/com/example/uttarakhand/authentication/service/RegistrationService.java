aa1be6ce5d1e03544ef21ba91b6adcc7636cf1d9
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-13 18:14:01 +0530

    Login Functionality

--- src/main/java/com/example/uttarakhand/authentication/service/RegistrationService.java
+++ src/main/java/com/example/uttarakhand/authentication/service/RegistrationService.java
@@ -45,9 +45,9 @@ public class RegistrationService {
         );
 
         String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
-//        emailSender.send(
-//                request.getEmail(),
-//                buildEmail(request.getFirstName(), link));
+        emailSender.send(
+                request.getEmail(),
+                buildEmail(request.getFirstName(), link));
 
         return "User registered successfully";
     }
