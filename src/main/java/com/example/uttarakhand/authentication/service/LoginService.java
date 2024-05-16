a53ea422d975bc5de78d5a133e398c067323960e
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-17 00:24:25 +0530

    Login Functionality

--- src/main/java/com/example/uttarakhand/authentication/service/LoginService.java
+++ src/main/java/com/example/uttarakhand/authentication/service/LoginService.java
@@ -1,8 +1,6 @@
 package com.example.uttarakhand.authentication.service;
 
 import com.example.uttarakhand.user.User;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.uttarakhand.user.UserRepository;
