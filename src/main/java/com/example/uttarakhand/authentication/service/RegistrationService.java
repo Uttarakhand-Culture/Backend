16b95dcf627a5e523c3c0a8a776d14565616645e
Ajay Negi <ajaynegi3345@icloud.com>
2024-06-01 18:21:55 +0530

    Login Functionality

--- src/main/java/com/example/uttarakhand/authentication/service/RegistrationService.java
+++ src/main/java/com/example/uttarakhand/authentication/service/RegistrationService.java
@@ -74,7 +74,8 @@ public class RegistrationService {
     }
 
     private String buildEmail(String name, String link) {
-    return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
             "\n" +
             "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
             "\n" +
